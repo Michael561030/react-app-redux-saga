c8114b6f5a5a0db9e9e28592244a79d2d262a38b
MaximjukMichael <maxmiha221@gmail.com>
2019-10-02 13:40:12 +0300

    creating react application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            products: []
+        };
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    componentDidMount() {
+        fetch("http://demo1656942.mockable.io/products.json")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        products: result.products
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, products } = this.state;
+        if (error) {
+            return <div>Помилка: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Завантаження...</div>;
+        } else {
+            return (
+                <ul>
+                    {products.map(product => (
+                        <li key={product.name}>
+                            <i>Name: </i>{product.brand} <br/>
+                            <i>Price: </i>{product.price} <br/>
+                            <i vertical-align="top">Photo: </i> <img src={product.img} alt={product.img} width="100px" height="100px"/>
+                            <hr/>
+                        </li>
+                    ))}
+                </ul>
+            );
+        }
+    }
 }
-
 export default App;
